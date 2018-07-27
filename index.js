b90be1be448a9102f01a76227f3ee2b92416670c
Oksana Tkachenko <oksana@crystax.net>
2018-07-27 20:05:05 +0300

    changed map

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
     pause: true,
     interval: 4000,
   });
+
+  ymaps.ready(init);
+
+  function init(){
+    var myMap = new ymaps.Map('map', {
+      center: [55.66599957, 37.75337950],
+      zoom: 16
+    });
+  }
 });
-
-ymaps.ready(init);
-
-function init(){
-  var myMap = new ymaps.Map('map', {
-    center: [55.66599957, 37.75337950],
-    zoom: 16
-  });
-}
