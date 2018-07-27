ff2ef19ef0a9e7065ed5473e7324b83c8fa2184e
Oksana Tkachenko <oksana@crystax.net>
2018-07-27 16:01:29 +0300

    WIP, added block sources

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
     pause: true,
     interval: 4000,
   });
+
+  $('.carouselSources[data-type='multi'] .item-sources').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<3;i++) {
+      next=next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
 });
