d75a5c479848dd10d70723d00af6669a1f70bdfa
Oksana Tkachenko <oksana@crystax.net>
2018-07-30 00:25:38 +0300

    changed carousel sources

--- index.js
+++ index.js
@@ -37,10 +37,13 @@ $(document).ready(function() {
     }
     next.children(':first-child').clone().appendTo($(this));
 
-    if (next.next().length>0) {
-      next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    for (var i=0;i<4;i++) {
+      next=next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+
+      next.children(':first-child').clone().appendTo($(this));
     }
   });
 
