928bfe89a2bc91002e2a9695102657723a6c02bf
Oksana Tkachenko <oksana@crystax.net>
2018-07-30 14:34:00 +0300

    changed tabs

--- index.js
+++ index.js
@@ -18,13 +18,46 @@ $(document).ready(function() {
       $('.navbar-menu').removeClass('stickytop');
     }
   });
-
+/*
   $('#myCarousel').carousel({
     pause: true,
     interval: 4000,
   });
 
   $('#myTab a[href="home"]').tab('show');
+*/
+  /* tabs */
+
+  var tab = $('#myTab');
+    var num = tab.find('.nav-tabs li').length - 1;
+    
+    $('.next').click(function(){
+        var cur = tab.find('li.active');
+        var index = cur.index();
+        
+        if(index == num) {
+            var next = tab.find('.nav-tabs').find('li').eq(0);
+        }
+        else {
+           var next = cur.next('li'); 
+        }
+        
+        next.find('a').tab('show');
+    });
+    
+    $('.prev').click(function(){
+        var cur = tab.find('li.active');
+        var index = cur.index();
+        
+        if(index == 0) {
+            var next = tab.find('.nav-tabs').find('li').eq(num);
+        }
+        else {
+           var next = cur.prev('li'); 
+        }
+        
+        next.find('a').tab('show');
+    }); 
 
   $('.multi-item-carousel').carousel({
     interval: false
